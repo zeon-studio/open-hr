286d447a8c791d62592f8e9c5342f198c0d5b054
teamosishq <teamosishq@gmail.com>
2025-01-25 15:37:11 +0600

    update asset page

--- src/app/(protected)/assets/_components/asset-preview.tsx
+++ src/app/(protected)/assets/_components/asset-preview.tsx
@@ -12,34 +12,32 @@ const AssetPreview = ({ assetData }: { assetData: Partial<TAsset> }) => {
       <div className="row justify-between items-center">
         <div className="lg:col-6 mb-4">
           <div className="font-medium mb-1">Name</div>
-          <div className="p-2 bg-gray-50 rounded">{assetData.name}</div>
+          <div className="p-2 bg-light rounded">{assetData.name}</div>
         </div>
         <div className="lg:col-6 mb-4">
           <div className="font-medium mb-1">Type</div>
-          <div className="p-2 bg-gray-50 rounded">{assetData.type}</div>
+          <div className="p-2 bg-light rounded">{assetData.type}</div>
         </div>
         <div className="lg:col-6 mb-4">
           <div className="font-medium mb-1">Price</div>
-          <div className="p-2 bg-gray-50 rounded">
+          <div className="p-2 bg-light rounded">
             {assetData.price}{" "}
             <span className="uppercase">{assetData.currency}</span>
           </div>
         </div>
         <div className="lg:col-6 mb-4">
           <div className="font-medium mb-1">User</div>
-          <div className="p-2 bg-gray-50 rounded">
+          <div className="p-2 bg-light rounded">
             {assetData.user ? employeeInfoById(assetData.user).name : "N/A"}
           </div>
         </div>
         <div className="lg:col-6 mb-4">
           <div className="font-medium mb-1">Serial Number</div>
-          <div className="p-2 bg-gray-50 rounded">
-            {assetData.serial_number}
-          </div>
+          <div className="p-2 bg-light rounded">{assetData.serial_number}</div>
         </div>
         <div className="lg:col-6 mb-4">
           <div className="font-medium mb-1">Purchase Date</div>
-          <div className="p-2 bg-gray-50 rounded">
+          <div className="p-2 bg-light rounded">
             {assetData.purchase_date
               ? new Date(assetData.purchase_date).toDateString()
               : "N/A"}
@@ -47,13 +45,11 @@ const AssetPreview = ({ assetData }: { assetData: Partial<TAsset> }) => {
         </div>
         <div className="lg:col-6 mb-4">
           <div className="font-medium mb-1">Status</div>
-          <div className="p-2 bg-gray-50 rounded">{assetData.status}</div>
+          <div className="p-2 bg-light rounded">{assetData.status}</div>
         </div>
-        <div className="col-12 mb-4">
+        <div className="lg:col-6 mb-4">
           <div className="font-medium mb-1">Note</div>
-          <div className="p-2 bg-gray-50 rounded">
-            {assetData.note || "N/A"}
-          </div>
+          <div className="p-2 bg-light rounded">{assetData.note || "N/A"}</div>
         </div>
 
         <div className="col-12">
@@ -61,21 +57,19 @@ const AssetPreview = ({ assetData }: { assetData: Partial<TAsset> }) => {
           {assetData.logs?.map((log, index) => (
             <div className="border mb-4 p-3 rounded" key={index}>
               <div className="row">
-                <div className="lg:col-4 mb-2">
+                <div className="lg:col-6 mb-2">
                   <div className="font-medium mb-1">Type</div>
-                  <div className="p-2 bg-gray-50 rounded">{log.type}</div>
+                  <div className="p-2 bg-light rounded">{log.type}</div>
                 </div>
-                <div className="lg:col-4 mb-2">
+                <div className="lg:col-6 mb-2">
                   <div className="font-medium mb-1">Date</div>
-                  <div className="p-2 bg-gray-50 rounded">
+                  <div className="p-2 bg-light rounded">
                     {new Date(log.date).toDateString()}
                   </div>
                 </div>
                 <div className="lg:col-12 mb-2">
                   <div className="font-medium mb-1">Description</div>
-                  <div className="p-2 bg-gray-50 rounded">
-                    {log.description}
-                  </div>
+                  <div className="p-2 bg-light rounded">{log.description}</div>
                 </div>
               </div>
             </div>
