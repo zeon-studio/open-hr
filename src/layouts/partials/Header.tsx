db8230303282a0ecc0003a6bf097c853ddbc243c
tfmukles <mukles.themefisher@gmail.com>
2025-01-26 16:24:57 +0600

    refactor: sidebar menu

--- src/layouts/partials/Header.tsx
+++ src/layouts/partials/Header.tsx
@@ -1,21 +1,36 @@
 import Logo from "@/components/logo";
 import { Button } from "@/components/ui/button";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { useDialog } from "@/hooks/useDialog";
 import { Menu } from "lucide-react";
 import Sidebar from "./Sidebar";
 
 const Header = () => {
+  const { isDialogOpen, onDialogChange } = useDialog();
   return (
     <header className="sticky px-4 h-[50px] top-0 lg:hidden flex justify-between items-center pb-5 border-b border-b-border/30 bg-light">
       <Logo />
-      <Sheet>
+      <Sheet open={isDialogOpen} onOpenChange={onDialogChange}>
         <SheetTrigger asChild>
           <Button size={"sm"} variant={"ghost"} className="p-2">
             <Menu />
           </Button>
         </SheetTrigger>
         <SheetContent side={"left"} className="max-w-xs">
-          <Sidebar />
+          <SheetHeader className="sr-only">
+            <SheetTitle>menu bar</SheetTitle>
+          </SheetHeader>
+          <Sidebar
+            onClose={() => {
+              onDialogChange(false);
+            }}
+          />
         </SheetContent>
       </Sheet>
     </header>
