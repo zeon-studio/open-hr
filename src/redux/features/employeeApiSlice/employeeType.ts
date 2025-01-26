8f08bee01e17992bfc3c0cdcf7be99c205653990
teamosishq <teamosishq@gmail.com>
2025-01-26 10:08:26 +0600

    update employee service

--- src/redux/features/employeeApiSlice/employeeType.ts
+++ src/redux/features/employeeApiSlice/employeeType.ts
@@ -21,6 +21,8 @@ export type TEmployee = {
   note: string;
   status: "pending" | "active" | "archived" | "";
   role: "user" | "moderator" | "admin" | "";
+  department?: "development" | "design" | "marketing" | "admin";
+  designation?: string;
   createdAt?: Date;
 };
 
