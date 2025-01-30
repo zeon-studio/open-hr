90fd1976d5b40ef16d089faedde3b642106115fa
teamosishq <teamosishq@gmail.com>
2025-01-30 12:53:38 +0600

    working on settings page

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -3,9 +3,6 @@
 import SettingPage from "./_components/setting-page";
 
 const Setting = () => {
-  // const { data } = useGetSettingQuery(undefined);
-  // const { result: settings } = data || {};
-
   return (
     <section className="p-8">
       <SettingPage />
