fbdca507b828157ecf6fb628b006de34a5ec2289
teamosishq <teamosishq@gmail.com>
2025-02-05 10:00:54 +0600

    update asset status and types

--- src/redux/features/assetApiSlice/assetType.ts
+++ src/redux/features/assetApiSlice/assetType.ts
@@ -1,5 +1,5 @@
 export type TAssetLog = {
-  type: "handover" | "return" | "repair" | "lost" | "damaged";
+  type: "handover" | "repair" | "takeover";
   description: string;
   date: Date;
 };
@@ -26,7 +26,7 @@ export type TAsset = {
   price: number;
   currency: "bdt" | "usd";
   purchase_date: Date;
-  status: "active" | "inactive" | "lost" | "damaged";
+  status: "active" | "inactive" | "lost" | "damaged" | "sold";
   note: string;
   logs: TAssetLog[];
   createdAt?: Date;
